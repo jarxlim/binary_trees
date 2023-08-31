e8c2546ac79e50738e5bd75d48821b3efb3da20f
chimachike1@gmail.com <chimachike1@gmail.com>
2023-08-31 09:29:54 -0700

    finshed task 25-31

--- 112-array_to_bst.c
+++ 112-array_to_bst.c
@@ -12,7 +12,7 @@ bst_t *array_to_bst(int *array, size_t size)
 	bst_t *root = NULL;
 
 	for (index = 0; index < size; index++)
-		bst_insert(&root, array[i]);
+		bst_insert(&root, array[index]);
 
 	return (root);
 }
