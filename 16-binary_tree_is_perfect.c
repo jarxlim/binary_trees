d22a73a205e247bad1fb50ade9e424f2b3eb184b
chimachike1@gmail.com <chimachike1@gmail.com>
2023-08-31 01:12:30 -0700

    done task 13-18

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -6,8 +6,8 @@
  * binary_tree_is_perfect - checks if a binary tree is perfect
  * @tree: pointer to the root node of the tree to check
  * Return: 1 if perfect,
- * 	0 otherwise.
- * 	If tree is NULL, return 0
+ *         0 otherwise.
+ *         If tree is NULL, return 0
  */
 
 int binary_tree_is_perfect(const binary_tree_t *tree)
